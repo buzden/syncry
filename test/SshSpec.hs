cf34066d121582aa16c5e53e8ab1b2350fe319f2
Grigoriy Volkov <gdvolkov@ispras.ru>
2019-08-02 18:20:56 +0300

    SSH: version takeTill

--- test/SshSpec.hs
+++ test/SshSpec.hs
@@ -29,6 +29,7 @@ import qualified Data.ByteString.Builder as B
 import qualified Data.Syntax.Printer.ByteString.Lazy as P
 import Test.HUnit.Lang (assertFailure)
 
+asciiL = BL.unpack . BC.pack
 ascii = fromList . BL.unpack . BC.pack
 
 shouldParseAs spec a b =
@@ -39,7 +40,7 @@ shouldParseAs spec a b =
 shouldGenerateAs spec a b =
    (B.toLazyByteString <$> P.runPrinter_ spec a) `shouldBe` Right b
 
-data Payload = Version (Vector Word8)
+data Payload = Version [Word8]
              | Ignore (Vector Word8)
              | ServiceRequest (Vector Word8)
              | ServiceAccept (Vector Word8)
@@ -49,17 +50,19 @@ $(makePrisms ''Payload)
 
 ssh2payload :: (SyntaxByte syn) => syn () Payload
 ssh2payload = version /+/ ignore /+/ servReq
-   where version = _Version /$/ word8 83 */ word8 83 */ word8 72 */ word8 45 */ word8 50 */ word8 46 */ word8 48 */ word8 45 */ vecN 4 anyWord8 -- takeWhile (const True)
+   -- BUG: version ... takeTill -- "not enough input"
+   -- BUG: version ... vecN 5 anyWord8 -- reverse order
+   where version = _Version /$/ word8 83 */ word8 83 */ word8 72 */ word8 45 */ word8 50 */ word8 46 */ word8 48 */ word8 45 */ (packed ^<< takeTill (== 10)) /* word8 10
          ignore = _Ignore /$/ word8 2 */ vecN 1 anyWord8 -- takeWhile (const True)
          servReq = _ServiceRequest /$/ word8 5 */ sizedByteSeq (anyWord32 LittleEndian)
 
 spec = describe "SSH spec" do
    it "parses" do
       let shouldParsePayload = shouldParseAs ssh2payload
-      "SSH-2.0-TesT" `shouldParsePayload` Version (ascii "TesT")
+      "SSH-2.0-TesT\r\n" `shouldParsePayload` Version (asciiL "TesT\r")
       "\x2_" `shouldParsePayload` Ignore (ascii "_")
-      "\x5\x4\0\0\0test" `shouldParsePayload` ServiceRequest (ascii "test")
+      "\x5\x6\0\0\0test" `shouldParsePayload` ServiceRequest (ascii "test")
 
    it "generates" do
       let shouldGeneratePayload = shouldGenerateAs ssh2payload
-      Version (ascii "TesT") `shouldGeneratePayload` "SSH-2.0-TesT"
+      Version (asciiL "TesT\r") `shouldGeneratePayload` "SSH-2.0-TesT\r\n"
