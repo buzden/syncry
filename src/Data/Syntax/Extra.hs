8a249a706cd1a894493a96258daa71b0a6785b18
Denis Buzdalov <public@buzden.ru>
2019-08-05 10:52:37 +0300

    Combinator for sequences with terminator was added.

--- src/Data/Syntax/Extra.hs
+++ src/Data/Syntax/Extra.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE TypeFamilies #-}
-module Data.Syntax.Extra (packed') where
+module Data.Syntax.Extra (packed', endingWith) where
+
+import Prelude hiding (takeWhile)
 
 import Control.Lens.Iso (Iso', iso)
+import Control.SIArrow ((/*))
 import Data.MonoTraversable (Element)
 import Data.Sequences (IsSequence)
-import Data.Syntax (packed)
+import Data.Syntax (Syntax, Seq, char, packed, takeWhile)
 import GHC.Exts (IsList, Item)
 import qualified GHC.Exts as IL (fromList, toList)
 
@@ -13,3 +16,6 @@ packed' = packed . listIsoIsList
 
 listIsoIsList :: (IsList l, Item l ~ a) => Iso' [a] l
 listIsoIsList = iso IL.fromList IL.toList
+
+endingWith :: Syntax syn => Element (Seq syn) -> syn () (Seq syn)
+endingWith terminator = takeWhile (/= terminator) /* char terminator
