b010111040d0e7e5e066d71ea9c36c6949d7c5d2
Denis Buzdalov <public@buzden.ru>
2019-08-05 12:11:40 +0300

    For some functions from `Syntax` polymorphic analogues were added.

--- src/Data/Syntax/Extra.hs
+++ src/Data/Syntax/Extra.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE TypeFamilies #-}
-module Data.Syntax.Extra (packed', endingWith) where
+module Data.Syntax.Extra (
+    packed',
+    take',
+    takeWhile', takeWhile1',
+    takeTill', takeTill1',
+    vecN',
+    endingWith) where
 
-import Prelude hiding (takeWhile)
+import Prelude hiding (take, takeWhile)
 
 import Control.Lens.Iso (Iso', iso)
-import Control.SIArrow ((/*))
+import Control.SIArrow ((/*), (>>^))
 import Data.MonoTraversable (Element)
 import Data.Sequences (IsSequence)
-import Data.Syntax (Syntax, Seq, char, packed, takeWhile)
+import Data.Syntax (Syntax, Seq, char, packed, take, takeTill, takeTill1, takeWhile, takeWhile1, vecN)
 import GHC.Exts (IsList, Item)
 import qualified GHC.Exts as IL (fromList, toList)
 
@@ -19,3 +25,21 @@ listIsoIsList = iso IL.fromList IL.toList
 
 endingWith :: Syntax syn => Element (Seq syn) -> syn () (Seq syn)
 endingWith terminator = takeWhile (/= terminator) /* char terminator
+
+take' :: (Syntax syn, IsList l, Item l ~ Element (Seq syn)) => Int -> syn () l
+take' n = take n >>^ packed'
+
+takeWhile' :: (Syntax syn, IsList l, Item l ~ Element (Seq syn)) => (Element (Seq syn) -> Bool) -> syn () l
+takeWhile' c = takeWhile c >>^ packed'
+
+takeWhile1' :: (Syntax syn, IsList l, Item l ~ Element (Seq syn)) => (Element (Seq syn) -> Bool) -> syn () l
+takeWhile1' c = takeWhile1 c >>^ packed'
+
+takeTill' :: (Syntax syn, IsList l, Item l ~ Element (Seq syn)) => (Element (Seq syn) -> Bool) -> syn () l
+takeTill' c = takeTill c >>^ packed'
+
+takeTill1' :: (Syntax syn, IsList l, Item l ~ Element (Seq syn)) => (Element (Seq syn) -> Bool) -> syn () l
+takeTill1' c = takeTill1 c >>^ packed'
+
+vecN' :: (Syntax syn, IsList l, Item l ~ a) => Int -> syn () a -> syn () l
+vecN' n a = vecN n a >>^ packed'
